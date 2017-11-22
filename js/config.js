3321b290ba21ea3878db0cf11c0310b2eb050761
Spencer McCoy <spencermccoy@unl.edu>
2017-11-22 14:47:29 -0600

    all the code

---
+++ js/config.js
@@ -0,0 +1,23 @@
+define({
+	logo: "https://ucomm.unl.edu/images/email-signature/Nebraska_N_RGB_small_on_white.gif", // (optional) Absolute URL of you logo. if no logo is provided the signature will be created without one. 
+	logoAlt: "Logo of the University of Nebraska &ndash; Lincoln", // Logo Alt Text. this is required if a logo image is provided
+	
+	API: "https://directory.unl.edu/people/{{user}}.json",	// (optional) this is the URI for UNL's directory. All fields are returned as arrays. if this is not provided default data will be used. {{user}} is replaced with what is entered in input box. 
+	
+	defaultData: { // (required) Data to be used if API does not return a field or if no API is provided. 
+		// use "fieldName: null" if a field should be omitted if not provided by the API. Otherwise the field is treated as fallback content. 
+		// Object keys are used to determine which fields should be pulled from the API. 
+		cn:["Herbie W. Husker"],
+		title:["Employee Title"],
+		organizationName: ["University of Nebraska&ndash;Lincoln"],
+	    unlHRPrimaryDepartment:["Department Name"],
+	    unlHRAddress:["1234 B street, Fakesville, USA"],
+	    telephoneNumber:["(555) 555-5555"]
+	},
+	dataTransforms: function(data){ // (optional) this function is called just before the data is used to generate the signature. You can use it to manipulate data. Here is an example removing the 's' from 'Communications'. You could potentially use it to shim an API that doesn't return data in Arrays. 
+		if(data.unlHRPrimaryDepartment){
+            data.unlHRPrimaryDepartment[0] = data.unlHRPrimaryDepartment[0].replace("Office of University Communications","Office of University Communication");    
+        }
+        return data
+	}
+})
